83e6760bac9ad7cf3d20ccc56361217edfdf8dd6
Shivam Tyagi <tshivam260@gmail.com>
2022-07-20 13:45:01 +0530

    #1-"First Commit"

---
+++ product/src/main/java/product/controller/MainController.java
@@ -0,0 +1,16 @@
+package product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/add")
+	public String addProduct() {
+		return "addproduct";
+	}
+	
+	//Adding Product form
+	
+}
---
+++ product/src/main/java/product/dao/ProductDao.java
@@ -0,0 +1,37 @@
+package product.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import product.model.Product;
+
+@Component
+public class ProductDao {
+
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+	
+	@Transactional
+	public void createProduct(Product product) {
+		this.hibernateTemplate.save(product);
+	}
+	
+	public List<Product> getALLProduct(){
+		List<Product> products=this.hibernateTemplate.loadAll(Product.class);
+		return products;
+	}
+	
+	@Transactional
+	public void deleteProduct(int pid) {
+		Product p=this.hibernateTemplate.load(Product.class, pid);
+		this.hibernateTemplate.delete(p);
+	}
+	
+	public Product getProduct(int pid) {
+		return this.hibernateTemplate.load(Product.class, pid);
+	}
+}
